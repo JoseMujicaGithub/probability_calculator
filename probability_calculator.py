96eac608535eaa8a721ee28cd9612313a678d18f
Jose Rafael Mujica <127058295+JoseMujicaGithub@users.noreply.github.com>
2024-08-18 13:18:02 -0400

    Add files via upload

--- probability_calculator.py
+++ probability_calculator.py
@@ -1,210 +1,119 @@
 import copy
 import random
 
+# Hat class definition
 class Hat:
+    def __init__(self, **kwargs):
+        # Initialize the Hat with balls of different colors.
+        self.contents = []
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
 
-  def __init__(self, **kwargs):
-    self.contents = []
-    for key, value in kwargs.items():
-      for i in range(value):
-        self.contents.append(key)
+    def draw(self, num):
+        # Draws a specified number of balls from the hat at random.
+        if num > len(self.contents):
+            return self.contents
+        draws = random.sample(self.contents, num)
+        for draw in draws:
+            self.contents.remove(draw)
+        return draws
 
-  def draw(self, num):
-    if num > len(self.contents):
-      return self.contents
-    draws = []
-
-    sample = random.sample(self.contents, num)
-    for x in sample:
-      draws.append(x)
-      self.contents.remove(x)
-    return draws
-
-
+# Function to perform the experiment and calculate the probability
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  succesfull_draw = 0
-  
-  drawed = []
-  global j
-  for j in range(num_experiments):
-    global expected_balls_copy 
-    expected_balls_copy= copy.deepcopy(expected_balls)
-    hat_copy = copy.deepcopy(hat)
-    drawed = hat_copy.draw(num_balls_drawn)
+    # Calculate the probability of drawing the expected balls in a series of experiments.
+    successful_draws = 0
 
-    drawedDict = {}
-    for x in drawed:
-      if x not in drawedDict:
-        drawedDict[x] = 1
-      else:
-        drawedDict[x] += 1
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn = hat_copy.draw(num_balls_drawn) # here the draw method is used with in the experiment function
+        
+        # Check if the drawn balls match or exceed the expected balls
+        if all(drawn.count(ball) >= count for ball, count in expected_balls.items()):
+            successful_draws += 1
 
-    allMatch = True
-    for key in expected_balls.keys():
-      if drawed.count(key) < expected_balls[key]:
-        allMatch = False
-        break
-    if allMatch:
-      succesfull_draw += 1
-  probability_=succesfull_draw / num_experiments
-  return probability_
+    return (successful_draws / num_experiments) * 100
 
-print("This progran calculates the probability of getting a certain combination of color balls")
-print("from a box full of ball of 5 diferent colors")
-
-colors_list=["Red",'Green','Blue',"Black","Pink"]
-
-def validate_color(input_description=": "):  
-    while True:
-        print(f"Yuo can choose fron thesse colors: \n{colors_list}")
-        color=(input(input_description)).lower().title()
-        if color in colors_list:
-            break
-        else:
-           print("==Invalid color==")
-    return color
-
-def validate_input_num(input_description=": "): 
+# Function to validate numeric input
+def validate_input_num(input_description=": "):
     while True:
         try:
-            num=int(input(input_description))
-        except:
-            print('===Invalid Input===')
-            continue
-        if type(num)==int:
+            num = int(input(input_description))
+            if num >= 0:
+                return num
+            else:
+                print('=== Input must be a non-negative integer ===')
+        except ValueError:
+            print('=== Invalid Input ===')
+
+# Function to calculate remaining balls distribution
+def calculate_remaining_balls_distribution(colors, num_balls):
+    # Randomly distribute the total number of balls among the specified colors.
+    ball_distribution = {}
+    remaining_balls = num_balls
+
+    for color in colors[:-1]:
+        ball_distribution[color] = random.randint(1, remaining_balls - (len(colors) - len(ball_distribution) - 1))
+        remaining_balls -= ball_distribution[color]
+
+    ball_distribution[colors[-1]] = remaining_balls
+    return ball_distribution
+
+# Main function to run the program
+def main():
+    # Define the number of balls
+    num_balls = validate_input_num("Enter the total number of balls in the hat: ")
+
+    # Define the available colors
+    colors = ["yellow", "blue", "green", "black"]
+
+    # Generate a random distribution of balls
+    ball_distribution = calculate_remaining_balls_distribution(colors, num_balls)
+    print(f"\nYour hat has the following ball distribution:\n{ball_distribution}")
+
+    # Define the number of balls to draw
+    while True:
+        num_balls_drawn = validate_input_num("Enter the number of balls to draw: ")
+        if num_balls_drawn <= num_balls:
             break
-    return num
-
-def validate_option(message="",max_option=1):
-  while True:
-    option_=input(message)
-    try: 
-      if int(option_)<=max_option and int(option_)>0:
-        break
-    except:
-      print("===Input a valid option===")
-  return option_
-
-max_balls=validate_input_num("Enter how many balls are in the box: ")
-#max_balls=45
-max_balls_copy1=max_balls
-max_balls_copy2=max_balls
-colors_list_copy_1=colors_list
-colors_list_copy_2=colors_list
-
-'''
-colors_list_sample=[]
-colors_dict={}
-while max_balls_copy1>0:
-   color=validate_color('Enter a color for the balls: ')
-   colors_list_sample.append(color)
-   #if color in colors_list_sample:
-   try:
-      if (colors_dict[color]):
-        option=validate_option(f"Do you want to overdrive the amoun of {color} balls?\n1 for Yes\n2 for No\n: ",2)
-      if option=='1':
-        max_balls_copy1+=colors_dict[color]
-        colors_dict[color]=validate_input_num(f'Enter the NEW amount of {color} balls: ')
-        print(f'overwrited color\n{colors_dict}')
-        if (max_balls_copy1-colors_dict[color])>=0:
-          max_balls_copy1-=colors_dict[color]
-          print(f'{max_balls_copy1} balls left to chosse')
         else:
-          del colors_dict[color]
-          print('This number excedes the amount of balls in the box')
-          print('==Select a diferent lower number==')
-      if option=='2':
-        skip=None
-   except:
-        colors_dict[color]=validate_input_num(f'Enter the amount of {color} balls: ')
-        if (max_balls_copy1-colors_dict[color])>=0:
-          max_balls_copy1-=colors_dict[color]
-          print(f'{max_balls_copy1} balls left to chosse')
-        else:
-          del colors_dict[color]
-          colors_list_sample.remove(color)
-          print('This number excedes the amount of balls in the box')
-          print('==Select a diferent lower number==')
+            print(f"=== The number exceeds the total amount of balls ({num_balls}) ===")
 
-print(f'user dict: \n {colors_dict}')
-'''
-#lest create a box of colored balls randomly 
-random_dict={}
-random_color=[]
-while max_balls_copy2>0:
-  if len(colors_list_copy_2)>1:
-    random_color.append(colors_list_copy_2.pop(random.randint(0,(len(colors_list_copy_2)-1))))
-    #print(f'max balls copy2: {max_balls_copy2}')
-  for element in random_color:
-    try:
-      if (random_dict[element]):
-        skip=None
-    except:
-      if len(colors_list)>1:
-        random_dict[element]=random.randint(0,max_balls_copy2)
-        max_balls_copy2-=random_dict.get(element)
-      else:
-        random_dict[element]=max_balls_copy2
-        if sum(random_dict.values())>max_balls:
-          colors_list_copy_2.append(element)
-          del random_dict[element]
-        else:
-          max_balls_copy2-=random_dict[element]
-          #print(f'max balls copy2 {max_balls_copy2}\n len de color_list: {len(colors_list_copy_2)}')
-        if len(colors_list_copy_2)==1:
-          random_color.append(colors_list_copy_2.pop(0))
-  for element in random_color:
-    if random_dict.get(element)==0:
-      del random_dict[element]
+    # If drawing all balls, the probability is 100%
+    if num_balls_drawn == num_balls:
+        print("The probability of drawing all colors is 100% when drawing the total amount of balls.")
+        return
 
-print(random_dict)
+    # Asking the user how many of each color they want to draw
+    expected_balls = {}
+    for color in colors:
+        max_draw = min(ball_distribution[color], num_balls_drawn)
+        while True:
+            expected_balls[color] = validate_input_num(f"Enter the number of {color} balls to draw (max {max_draw}): ")
+            if expected_balls[color] <= max_draw:
+                num_balls_drawn -= expected_balls[color]
+                break
+            else:
+                print(f"=== The number of {color} balls to draw must not exceed {max_draw} ===")
 
-#colors_list=["Red",'Green','Blue',"Black","Pink"]
+        # If all balls to be drawn have been assigned, break out of the loop
+        if num_balls_drawn == 0:
+            break
 
-try:
-   red=random_dict["Red"]
-except:
-   red=0
-try:
-   green=random_dict['Green']
-except:
-   green=0
-try:
-   blue=random_dict['Blue']
-except:
-   blue=0
-try:
-   black=random_dict["Black"]
-except:
-   black=0
-try:
-   pink=random_dict["Pink"]
-except:
-   pink=0
+    # Validate that the sum of expected balls does not exceed the number of balls to draw
+    if sum(expected_balls.values()) > num_balls:
+        print(f"=== The total number of balls to draw exceeds {num_balls} ===")
+        return
 
-print(f'red={red} green={green} blue={blue} black={black} pink={pink}')
+    # Define the number of experiments
+    num_experiments = validate_input_num("Enter the number of experiments to perform: ")
 
-box=Hat(Red=red,Green=green,Blue=blue,Black=black,Pink=pink)
-#print(f'this is the box: {box}') ===========passed===========
+    # Create the Hat object
+    hat = Hat(**ball_distribution)
 
-while True:
-  num_balls_to_drawn=validate_input_num(f'Enter how many balls you want to drwa from the box of {max_balls}: ')
-  draw=box.draw(num_balls_to_drawn)
-  if max_balls >= num_balls_to_drawn:
-    break
-  else:
-    print(f"The number of balls to draw must be less or equal to the amount of balls in the box ({max_balls})")
-experiments=validate_input_num('Enter how many experiments you wish to do: ')
-#{"Red": 2,"Green": 3}
-'''
-probability = prob_calculator.experiment(hat=hat,
-                                             expected_balls={
-                                                 "yellow": 2,
-                                                 "blue": 3,
-                                                 "test": 1
-                                             },
-                                             num_balls_drawn=20,
-                                             num_experiments=10000)
-'''
-probability=experiment(hat=box,expected_balls=random_dict,num_balls_drawn=num_balls_to_drawn,num_experiments=experiments)
-print(probability)
+    # Calculate and display the probability
+    probability = experiment(hat, expected_balls, sum(expected_balls.values()), num_experiments)
+    print(f"\nProbability of drawing the expected balls: {probability:.2f}%")
+
+# Run the main function
+if __name__ == "__main__":
+    main()
